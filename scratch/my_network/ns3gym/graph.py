93bde3944133c135a62e571b0219934ac54e511e
Tiago <tsb4@cin.ufpe.br>
2021-12-15 12:11:50 +0100

    Adding a routing table function

--- scratch/my_network/ns3gym/graph.py
+++ scratch/my_network/ns3gym/graph.py
@@ -1,12 +1,15 @@
 from queue import PriorityQueue
 
 class Graph:
-    def __init__(self, num_of_vertices):
+    def __init__(self, num_of_vertices, start_vertex):
         self.v = num_of_vertices
         self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
         self.visited = []
         self.D = None
+        self.Parent = None
+        self.RoutingTable = None
         self.adjacency_matrix_file_name = 'adjacency_matrix.txt'
+        self.start_vertex = start_vertex
     
     def openFile(self):
         f = open('../'+self.adjacency_matrix_file_name, 'r')
@@ -21,12 +24,14 @@ class Graph:
         self.edges[u][v] = weight
         self.edges[v][u] = weight
 
-    def dijkstra(self, start_vertex):
+    def dijkstra(self):
         self.D = {v:float('inf') for v in range(self.v)}
-        self.D[start_vertex] = 0
+        self.Parent = {v:float(self.start_vertex) for v in range(self.v)}
+        self.RoutingTable = {v:float(-1) for v in range(self.v)}
+        self.D[self.start_vertex] = 0
 
         pq = PriorityQueue()
-        pq.put((0, start_vertex))
+        pq.put((0, self.start_vertex))
 
         while not pq.empty():
             (dist, current_vertex) = pq.get()
@@ -41,4 +46,20 @@ class Graph:
                         if new_cost < old_cost:
                             pq.put((new_cost, neighbor))
                             self.D[neighbor] = new_cost
+                            if(current_vertex==self.start_vertex):
+                                self.Parent[neighbor] = neighbor
+                            else:
+                                self.Parent[neighbor] = self.Parent[current_vertex]
         return self.D
+    
+    def getRoutingTable(self):
+        for i in range(self.v):
+            count=0
+            for j in range(self.v):
+                if(j==self.Parent[i]):
+                    break
+                if(i==j):
+                    continue
+                if(self.edges[self.start_vertex][j]==1):
+                    count += 1
+            self.RoutingTable[i] = count
