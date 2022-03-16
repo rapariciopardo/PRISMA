5204f01ec4291117d6c5820b32ab942f21cad381
Tiago <tsb4@cin.ufpe.br>
2022-03-16 09:39:33 +0100

    Added Geant topology, fix launch_sp script, adding normalized traffic rate matrix for geant

--- my_network/launch_sp.sh
+++ my_network/launch_sp.sh
@@ -1,76 +1,48 @@
 #!/bin/bash  
-
-## Testing dqn
-## Uncoment the lines below 
-#couter=0
-#batchSize=10
-#for ((i=0; i<4; i++))
-#	do  for ((j=100; j<=500; j=j+100))
-#		do 
-#		res=$((counter%batchSize))
-#		if (($res==0)); then
-#			wait
-#		fi
-#		sleep $i
-#		python3 multi_agents_threaded.py --simTime=60 \
-#			--basePort=$((6555+$j + (15*$i))) \
-#			--session_name="dqn_mat_$(($i))_seed_$j" \
-#			--logs_parent_folder=outputs/test_dqn_buffer_no_signaling/ \
-#			--seed=100 \
-#			--start_tensorboard=0 \
-#			--train=0 \
-#			--max_nb_arrived_pkts=10000 \
-#			--load_path=saved_models/dqn_mat_$(($i))_seed_$j/episode1 \
-#			--save_models=0 \
-#			--exploration_initial_eps=0 \
-#			--exploration_final_eps=0 \
-#			--load_factor=0.01&
-#		counter=$((counter+"1"))
-#	done
-#done
-
+cp -r ../my_network ../ns3-gym/scratch
+cd ../ns3-gym
+./waf configure && ./waf build
+sleep 3
+cd ../my_network
 
 ## Test SP with load factor variation
 ## Uncomment the lines below
-#array=(
-#0.0005
-#0.001
-#0.005
-#0.01
-#0.015
-#0.02
-#0.025
-#0.03
-#)
+array=(
+0.0039
+0.1
+0.3
+)
 #counter=0
 #batchSize=10
-#for ((i=0; i<4; i++))
-#	do  for j in ${array[@]}
-#		do 
-#		res=$((counter%batchSize))
-#		if (($res==0)); then
-#			wait
-#		fi
-#		sleep $i
-#		res1=$(printf "%.0f" $(echo "scale=2; ($j  / 0.01 * 100 )" | bc ))
-#		echo $res1
-#		python3 multi_agents_threaded.py --simTime=600 \
-#			--basePort=$((6555 + (15*counter))) \
-#			--session_name="sp_mat_$(($i))_load_$j" \
-#			--logs_parent_folder=outputs/tests/mat_$(($i))/load_$(($res1))/sp/ \
-#			--seed=100 \
-#			--start_tensorboard=0 \
-#			--train=0 \
-#			--agent_type=sp \
-#			--max_nb_arrived_pkts=10000 \
-#			--save_models=0 \
-#			--exploration_initial_eps=0 \
-#			--exploration_final_eps=0 \
-#			--traffic_matrix_path=scratch/my_network/examples/abilene/traffic_matrices/node_intensity_$(($i)).txt \
-#			--load_factor=$j &		
-#		counter=$((counter+"1"))
-#	done
-#done
+for ((i=0; i<1; i++))
+	do  for j in ${array[@]}
+		do 
+		#res=$((counter%batchSize))
+		#if (($res==0)); then
+		#	wait
+		#fi
+		sleep $i
+        FLOAT=$(echo $j*10000 | bc)
+        res1=${FLOAT/.*}
+		echo $res1
+		python3 multi_agents_threaded.py --simTime=600 \
+			--basePort=$((6655 )) \
+			--session_name="sp_mat_$(($i))_load_$res1" \
+			--logs_parent_folder=examples/geant/outputs/mat_$(($i))/load_$(($res1))/sp/ \
+			--seed=100 \
+			--start_tensorboard=0 \
+			--train=0 \
+			--agent_type=sp \
+			--max_nb_arrived_pkts=10000 \
+			--save_models=0 \
+			--exploration_initial_eps=0 \
+			--exploration_final_eps=0 \
+			--traffic_matrix_path=scratch/my_network/examples/geant/traffic_matrices/node_intensity_$(($i)).txt \
+			--load_factor=$j &		
+		counter=$((counter+"1")) &
+        sleep 5
+	done
+done
 
 
 ## test DQN with load factor variation
